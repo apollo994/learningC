8688058660a6a2343d952a5e44dfbd8d4901239a
apollo994 <fabio.zanarello.94@gmail.com>
2025-04-10 17:37:25 +0200

    fix 1.6

--- chapter1/1.6.IO.c
+++ chapter1/1.6.IO.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-int main() 
-{ 
-    char c; 
-
-    while ((c = (getchar() != EOF))) 
-    {
-        putchar(c);
-        printf("%c", c);
-    }
+int main()
+{
+    int c = (getchar() != EOF);
+    printf("%i\n",c);
 }
